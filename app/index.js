0ed00d216c4f250396bc129719d5d9cc33c56ea1
obuastella <stellaobua123@gmail.com>
2026-02-28 10:49:31 +0100

    setup db config and start

--- app/index.js
+++ app/index.js
@@ -1,12 +1,22 @@
-const express = require("express");
-
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 const app = express();
-const PORT = 3000;
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on Port: ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on Port: ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("An error occured while starting the server....");
+  }
+};
+
+startServer();
