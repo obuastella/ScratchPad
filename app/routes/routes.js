cc245b171caf4a630efd33604139ae005059b93a
obuastella <stellaobua123@gmail.com>
2026-03-03 12:22:30 +0100

    Start from scratch

--- app/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express, { Router } from "express";
-import notesController from "../controllers/notes.controller.js";
-
-const router = express.Router();
-
-router.get("/notes", notesController);
-
-export default router;
