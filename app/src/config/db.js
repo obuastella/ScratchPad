bf4e9ca7d5141ae793136d13699a5840865222e6
obuastella <stellaobua123@gmail.com>
2026-03-04 20:44:25 +0100

    connect to db

---
+++ app/src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to db...");
+  } catch (error) {
+    console.log("Error connecting to db..", error);
+  }
+};
+
+export default connectDB;
--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,11 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 const startServer = async () => {
   try {
+    await connectDB();
     app.listen(() => {
       console.log(`Listening on Port: `, process.env.PORT);
     });
