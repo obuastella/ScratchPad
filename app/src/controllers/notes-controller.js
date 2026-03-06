62b360f683517111c7da87045c664c69bb236e21
obuastella <stellaobua123@gmail.com>
2026-03-06 23:40:28 +0100

    update note

--- app/src/controllers/notes-controller.js
+++ app/src/controllers/notes-controller.js
@@ -34,4 +34,26 @@ const createNote = async (req, res) => {
   }
 };
 
-export { getAllNotes, createNote };
+const updateNote = async (req, res) => {
+  try {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      res.status(404).json({ message: "Invalid note id" });
+    }
+    const note = await Notes.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!note) {
+      res.status(404).json({ message: "Note not found" });
+    }
+    res.status(201).json({ message: "Note updated succesfully", note: note });
+  } catch (error) {
+    if (error === "CastError") {
+      res.status(404).json({ message: "Invalid id format" });
+    }
+    res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
+  }
+};
+
+export { getAllNotes, createNote, updateNote };
--- app/src/routes/notes-routes.js
+++ app/src/routes/notes-routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getAllNotes, createNote } from "../controllers/notes-controller.js";
+import {
+  getAllNotes,
+  createNote,
+  updateNote,
+} from "../controllers/notes-controller.js";
 
 const router = Router();
 
 router.get("/", getAllNotes);
 router.post("/", createNote);
+router.patch("/:id", updateNote);
 
 export default router;
